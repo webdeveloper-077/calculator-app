7d157821a349f3cbf6da0b668a4efc2486830635
webdeveloper-077 <121479148+webdeveloper-077@users.noreply.github.com>
2023-02-18 18:36:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// index.js
+import React from 'react'
+import App from './App'
+import ReactDOM from 'react-dom';
+import "./style.css";
+
+ReactDOM.render(<App/>, document.getElementById('root'));
